88a2a787a26eb0b8498a026ad40f459173d42503
bhavika79 <71648869+bhavika79@users.noreply.github.com>
2021-05-31 17:02:56 +0530

    3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!,Welcome everyone";
+    private final String message = "Hello World!,Welcome everyone1";
 
     public App() {}
 
